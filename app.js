a8c946822ae03ae377c1540149372f8b963b3d26
davidOC007 <75745959+davidOC007@users.noreply.github.com>
2021-06-21 11:59:11 +0200

    Modification du fichier app.js

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const helmet = require ('helmet');
+const bodyParser = require('body-parser');
+const path = require('path');
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvcrj.mongodb.net/${process.env.DB_CLUSTER}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-module.exports = app;
+  app.use(bodyParser.json());
+
+  app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Content-Type, Authorization');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+      next();
+  });
+  
+  app.use(helmet());
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+  app.use('/api/auth', userRoutes);
+  app.use('/api/sauces', sauceRoutes);
+  
+  module.exports = app;
